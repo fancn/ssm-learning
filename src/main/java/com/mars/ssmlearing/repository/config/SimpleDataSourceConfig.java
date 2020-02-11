7ecf0d9e76e8031eee5c7d7e1e945467b3d9de11
fancn <fcn11@outlook.com>
2020-02-11 21:32:11 +0800

    add jdbcTemplate

--- src/main/java/com/mars/ssmlearing/repository/config/SimpleDataSourceConfig.java
+++ src/main/java/com/mars/ssmlearing/repository/config/SimpleDataSourceConfig.java
@@ -1,8 +1,10 @@
 package com.mars.ssmlearing.repository.config;
 
 import com.mysql.cj.jdbc.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import java.sql.SQLException;
@@ -10,9 +12,13 @@ import java.sql.SQLException;
 @Configuration
 public class SimpleDataSourceConfig {
 
-    @Bean
+    @Bean("simpleDataSource")
     public SimpleDriverDataSource simpleDataSource() throws SQLException {
         return new SimpleDriverDataSource(new Driver(), "jdbc:mysql://172.22.5.5:3306/gear", "tester", "123456");
     }
 
+    @Bean("jdbcTemplate")
+    public JdbcTemplate buildJdbcTemplate(@Autowired SimpleDriverDataSource simpleDataSource) {
+        return new JdbcTemplate(simpleDataSource);
+    }
 }
--- src/test/java/com/mars/ssmlearing/SsmLearingApplicationTests.java
+++ src/test/java/com/mars/ssmlearing/SsmLearingApplicationTests.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @SpringBootTest
@@ -13,12 +14,16 @@ class SsmLearingApplicationTests {
     @Autowired
     private SimpleDriverDataSource simpleDriverDataSource;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     private static final Logger logger = LoggerFactory.getLogger(SsmLearingApplicationTests.class);
 
     @Test
     public void contextLoads() {
         String a = simpleDriverDataSource.getUrl();
         logger.info(a);
+        logger.info("" + jdbcTemplate.getQueryTimeout());
     }
 
 }
